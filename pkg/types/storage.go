9f175b8568ff926fb81a755823b765bba43e0f53
hybfkuf <fredjoseph863@gmail.com>
2022-08-15 16:50:31 +0800

    Add package "types"

---
+++ pkg/types/storage.go
@@ -0,0 +1,10 @@
+package types
+
+const (
+	StorageNFS        = "nfs"
+	StorageCephFS     = "cephfs"
+	StorageS3         = "s3"
+	StorageRestServer = "restServer"
+	StorageSFTP       = "sftp"
+	StorageRClone     = "rclone"
+)
