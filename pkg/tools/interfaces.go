d044972a25b80e3e3227c1d795be6199428b3e93
hybfkuf <fredjoseph863@gmail.com>
2022-09-13 14:17:51 +0800

    Add Backup()

--- pkg/tools/interfaces.go
+++ pkg/tools/interfaces.go
@@ -1,7 +1,11 @@
 package tools
 
+import (
+	storagev1alpha1 "github.com/forbearing/horus-operator/apis/storage/v1alpha1"
+)
+
 type BackupInterface interface {
-	DoBackup(dst, src string) error
+	DoBackup(*storagev1alpha1.BackupFrom, *storagev1alpha1.BackupTo) error
 }
 
 type RestoreInterface interface {
@@ -18,4 +22,8 @@ type CloneInterface interface {
 
 type TrafficInterface interface {
 	DoTraffic()
+}
+
+func Backup(b BackupInterface, backupFrom *storagev1alpha1.BackupFrom, backupTo *storagev1alpha1.BackupTo) error {
+	return b.DoBackup(backupFrom, backupTo)
 }
