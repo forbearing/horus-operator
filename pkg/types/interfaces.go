802cfa57d51c60580340c2977557302f3b55fd3c
hybfkuf <fredjoseph863@gmail.com>
2022-09-15 19:35:15 +0800

    Update Interface

--- pkg/types/interfaces.go
+++ pkg/types/interfaces.go
@@ -1,26 +1,26 @@
 package types
 
 import (
-	storagev1alpha1 "github.com/forbearing/horus-operator/apis/storage/v1alpha1"
 	networkingv1alpha1 "github.com/forbearing/horus-operator/apis/networking/v1alpha1"
+	storagev1alpha1 "github.com/forbearing/horus-operator/apis/storage/v1alpha1"
 )
 
 type BackupInterface interface {
-	DoBackup(backupObj *storagev1alpha1.Backup) error
+	Backup(backupObj *storagev1alpha1.Backup) error
 }
 
 type RestoreInterface interface {
-	DoRestore(restoreObj *storagev1alpha1.Restore) error
+	Restore(restoreObj *storagev1alpha1.Restore) error
 }
 
 type MigrationInterface interface {
-	DoMigration(migrationObj *storagev1alpha1.Migration) error
+	Migration(migrationObj *storagev1alpha1.Migration) error
 }
 
 type CloneInterface interface {
-	DoClone(cloneObj *storagev1alpha1.Clone) error
+	Clone(cloneObj *storagev1alpha1.Clone) error
 }
 
 type TrafficInterface interface {
-	DoTraffic(trafficObj *networkingv1alpha1.Traffic)
+	Traffic(trafficObj *networkingv1alpha1.Traffic)
 }
