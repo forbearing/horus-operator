63b209201b3f8a5d678af3e9b4038619f4233114
hybfkuf <fredjoseph863@gmail.com>
2022-09-15 19:35:46 +0800

    Update crojob method

--- pkg/cronjob/cronjob.go
+++ pkg/cronjob/cronjob.go
@@ -7,3 +7,19 @@ import (
 func AddBackup(types.BackupInterface)    {}
 func DeleteBackup(types.BackupInterface) {}
 func UpdateBackup(types.BackupInterface) {}
+
+func AddRestore(types.RestoreInterface)    {}
+func DeleteRestore(types.RestoreInterface) {}
+func UpdateRestore(types.RestoreInterface) {}
+
+func AddMigration(types.MigrationInterface)    {}
+func DeleteMigration(types.MigrationInterface) {}
+func UpdateMigration(types.MigrationInterface) {}
+
+func AddClone(types.CloneInterface)    {}
+func DeleteClone(types.CloneInterface) {}
+func UpdateClone(types.CloneInterface) {}
+
+func AddTraffic(types.TrafficInterface)    {}
+func DeleteTraffic(types.TrafficInterface) {}
+func UpdateTraffic(types.TrafficInterface) {}
