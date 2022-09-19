f110d7baf5bb77e622a8c56663e9bc1fc3357c0c
hybfkuf <fredjoseph863@gmail.com>
2022-09-19 16:57:33 +0800

    Add package template

---
+++ pkg/template/deployment_backup2minio.go
@@ -0,0 +1,73 @@
+package template
+
+var (
+	Backup2minioDeploymentTemplate = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: "%s"
+  namespace: "%s"
+  labels:
+    app.kubernetes.io/name: backup-to-minio
+    app.kubernetes.io/part-of: horus-operator
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: backup-to-minio
+      app.kubernetes.io/part-of: horus-operator
+  template:
+    metadata:
+      #annotations:
+      #  %s: %s
+      labels:
+        app.kubernetes.io/name: backup-to-minio
+        app.kubernetes.io/part-of: horus-operator
+    spec:
+      nodeName: "%s"
+      tolerations:
+      - operator: Exists
+      containers:
+      - name: backup-to-minio
+        image: "%s"
+        env:
+        - name: TZ
+          value: %s
+        - name: RESTIC_REPOSITORY
+          value: %s
+        - name: RESTIC_PASSWORD
+          valueFrom:
+            secretKeyRef:
+              name: %s
+              key: RESTIC_PASSWORD
+        - name: MINIO_ACCESS_KEY
+          valueFrom:
+            secretKeyRef:
+              name: %s
+              key: MINIO_ACCESS_KEY
+        - name: MINIO_SECRET_KEY
+          valueFrom:
+            secretKeyRef:
+              name: %s
+              key: MINIO_SECRET_KEY
+        - name: AWS_ACCESS_KEY_ID
+          valueFrom:
+            secretKeyRef:
+              name: %s
+              key: MINIO_ACCESS_KEY
+        - name: AWS_SECRET_ACCESS_KEY
+          valueFrom:
+            secretKeyRef:
+              name: %s
+              key: MINIO_SECRET_KEY
+        volumeMounts:
+        - name: host-root
+          mountPath: /host-root
+          readOnly: true
+      volumes:
+      - name: host-root
+        hostPath:
+          path: /
+          type: Directory
+`
+)
---
+++ pkg/template/deployment_findpvdir.go
@@ -0,0 +1,50 @@
+package template
+
+var (
+	FindpvdirDeploymentTemplate = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: %s
+  namespace: %s
+  labels:
+    app.kubernetes.io/name: findpvdir
+    app.kubernetes.io/part-of: horus-operator
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: findpvdir
+      app.kubernetes.io/part-of: horus-operator
+  template:
+    metadata:
+      #annotations:
+      #  %s: %s
+      labels:
+        app.kubernetes.io/name: findpvdir
+        app.kubernetes.io/part-of: horus-operator
+    spec:
+      nodeName: %s
+      tolerations:
+      - operator: Exists
+      containers:
+      - name: findpvdir
+        image: %s
+        env:
+        - name: TZ
+          value: %s
+        resources:
+          limits:
+            cpu: 0.1
+            memory: 50Mi
+        volumeMounts:
+        - name: kubelet-home-dir
+          mountPath: /var/lib/kubelet
+          readOnly: true
+      volumes:
+      - name: kubelet-home-dir
+        hostPath:
+          path: /var/lib/kubelet
+          type: Directory
+`
+)
