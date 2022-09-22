2d28a91d6b18d6c354323d5628039e8595cc1185
hybfkuf <fredjoseph863@gmail.com>
2022-09-22 17:07:58 +0800

    package util

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"os"
+
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -8,4 +10,25 @@ import (
 func GetPodPVPath(p *corev1.Pod) []string {
 
 	return nil
+}
+
+const (
+	defaultOperatorNamespace = "horus-operator"
+	namespaceEnv             = "NAMESPACE"
+	namespaceFile            = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+)
+
+// GetOperatorNamespace
+func GetOperatorNamespace() string {
+	var namespace string
+	if namespace = os.Getenv(namespaceEnv); len(namespace) != 0 {
+		return namespace
+	}
+	// namespace file exists and is not direcotry
+	if fs, err := os.Stat(namespaceFile); err == nil && !fs.IsDir() {
+		if data, err := os.ReadFile(namespaceFile); err == nil {
+			return string(data)
+		}
+	}
+	return defaultOperatorNamespace
 }
