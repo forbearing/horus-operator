3f1f594118f287efcbccada7363f96568e218598
hybfkuf <fredjoseph863@gmail.com>
2022-09-23 15:37:17 +0800

    Update GetOperatorNamespace() and add GetCurrentNamespace()

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -14,21 +14,28 @@ func GetPodPVPath(p *corev1.Pod) []string {
 }
 
 const (
-	namespaceEnv  = "NAMESPACE"
-	namespaceFile = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+	EnvOperatorNamespace = "OPERATOR_NAMESPACE"
+	EnvWatchNamespace    = "WATCH_NAMESPACE"
+	EnvCurrentNamespace  = "NAMESPACE"
+	namespaceFile        = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
 )
 
-// GetOperatorNamespace
-func GetOperatorNamespace() string {
-	var namespace string
-	if namespace = os.Getenv(namespaceEnv); len(namespace) != 0 {
-		return namespace
-	}
+// GetCurrentNamespace
+func GetCurrentNamespace() string {
 	// namespace file exists and is not direcotry
 	if fs, err := os.Stat(namespaceFile); err == nil && !fs.IsDir() {
 		if data, err := os.ReadFile(namespaceFile); err == nil {
 			return string(data)
 		}
+	}
+	return os.Getenv(EnvCurrentNamespace)
+}
+
+// GetOperatorNamespace
+func GetOperatorNamespace() string {
+	var namespace string
+	if namespace = os.Getenv(EnvOperatorNamespace); len(namespace) != 0 {
+		return namespace
 	}
 	return types.DefaultOperatorNamespace
 }
