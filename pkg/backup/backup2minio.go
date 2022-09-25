5e552c5f0b6a07199a175f4f84965e337924be56
hybfkuf <fredjoseph863@gmail.com>
2022-09-25 16:05:31 +0800

    Wait k8s object ready and available before start backup

--- pkg/backup/backup2minio.go
+++ pkg/backup/backup2minio.go
@@ -1,10 +1,10 @@
 package backup
 
 import (
+	"fmt"
 	"time"
 
 	storagev1alpha1 "github.com/forbearing/horus-operator/apis/storage/v1alpha1"
-	"github.com/forbearing/horus-operator/pkg/types"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -16,26 +16,53 @@ import (
 //     - "restic list keys" check whether resitc repository exist
 //     - "restic init" initial a resitc repository when repository not exist.
 //     - "restic backup" backup the persistentvolume data to nfs storage.
-func Backup2MinIO(backupObj *storagev1alpha1.Backup, pvc string, meta pvdataMeta) (time.Duration, error) {
+func Backup2MinIO(backupObj *storagev1alpha1.Backup, pvc string, meta pvdataMeta) error {
 	beginTime := time.Now()
 	logger := logrus.WithFields(logrus.Fields{
-		"Component": "Backup",
+		"Component": "Backup2MinIO",
 		"Tool":      "Restic",
 		"Storage":   "MinIO",
 	})
 
 	var err error
+	switch backupObj.Spec.BackupFrom.Resource {
+	case storagev1alpha1.PodResource:
+		if err = podHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("pod handler wait pod/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	case storagev1alpha1.DeploymentResource:
+		if err = depHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("deployment handler wait deployment/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	case storagev1alpha1.StatefulSetResource:
+		if err = stsHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("statefulset handler wait statefulset/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	case storagev1alpha1.DaemonSetResource:
+		if err = dsHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("daemonset handler wait daemonset/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	default:
+		err := fmt.Errorf("not support backup resource: %s", backupObj.Spec.BackupFrom.Resource)
+		return err
+	}
+
 	var costedTime time.Duration
 	var execPod *corev1.Pod
 	if execPod, costedTime, err = createBackup2minioDepoyment(backupObj, meta); err != nil {
-		return time.Now().Sub(beginTime), err
+		return err
 	}
 	logger.WithFields(logrus.Fields{"Cost": costedTime.String()}).Debugf("create deployment/%s", backup2minioName+"-"+meta.nodeName)
 
 	// execute restic command to backup persistentvolume data to remote storage within the pod.
-	if costedTime, err = executeBackupCommand(backupObj, execPod, pvc, meta, types.StorageMinIO); err != nil {
-		return time.Now().Sub(beginTime), err
+	if costedTime, err = executeBackupCommand(backupObj, execPod, pvc, meta); err != nil {
+		return err
 	}
+	costedTime = time.Now().Sub(beginTime)
 	logger.WithFields(logrus.Fields{"Cost": costedTime.String()}).Infof("Successfully backup pvc/%s", pvc)
-	return time.Now().Sub(beginTime), nil
+	return nil
 }
--- pkg/backup/backup2nfs.go
+++ pkg/backup/backup2nfs.go
@@ -1,10 +1,10 @@
 package backup
 
 import (
+	"fmt"
 	"time"
 
 	storagev1alpha1 "github.com/forbearing/horus-operator/apis/storage/v1alpha1"
-	"github.com/forbearing/horus-operator/pkg/types"
 	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 )
@@ -17,26 +17,53 @@ import (
 //     - "restic list keys" check whether resitc repository exist
 //     - "restic init" initial a resitc repository when repository not exist.
 //     - "restic backup" backup the persistentvolume data to nfs storage.
-func Backup2NFS(backupObj *storagev1alpha1.Backup, pvc string, meta pvdataMeta) (time.Duration, error) {
+func Backup2NFS(backupObj *storagev1alpha1.Backup, pvc string, meta pvdataMeta) error {
 	beginTime := time.Now()
 	logger := logrus.WithFields(logrus.Fields{
-		"Component": "Backup",
+		"Component": "Backup2NFS",
 		"Tool":      "Restic",
 		"Storage":   "NFS",
 	})
 
 	var err error
+	switch backupObj.Spec.BackupFrom.Resource {
+	case storagev1alpha1.PodResource:
+		if err = podHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("pod handler wait pod/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	case storagev1alpha1.DeploymentResource:
+		if err = depHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("deployment handler wait deployment/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	case storagev1alpha1.StatefulSetResource:
+		if err = stsHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("statefulset handler wait statefulset/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	case storagev1alpha1.DaemonSetResource:
+		if err = dsHandler.WithNamespace(backupObj.GetNamespace()).WaitReady(backupObj.Spec.BackupFrom.Name); err != nil {
+			logger.Errorf("daemonset handler wait daemonset/%s to be ready failed: %s", backupObj.Spec.BackupFrom.Name, err.Error())
+			return err
+		}
+	default:
+		err := fmt.Errorf("not support backup resource: %s", backupObj.Spec.BackupFrom.Resource)
+		return err
+	}
+
 	var costedTime time.Duration
 	var execPod *corev1.Pod
 	if execPod, costedTime, err = createBackup2nfsDeployment(backupObj, meta); err != nil {
-		return time.Now().Sub(beginTime), err
+		return err
 	}
 	logger.WithFields(logrus.Fields{"Cost": costedTime.String()}).Debugf("create deployment/%s", backup2nfsName+"-"+meta.nodeName)
 
 	// execute restic command to backup persistentvolume data to remote storage within the pod.
-	if costedTime, err = executeBackupCommand(backupObj, execPod, pvc, meta, types.StorageNFS); err != nil {
-		return time.Now().Sub(beginTime), err
+	if costedTime, err = executeBackupCommand(backupObj, execPod, pvc, meta); err != nil {
+		return err
 	}
+	costedTime = time.Now().Sub(beginTime)
 	logger.WithFields(logrus.Fields{"Cost": costedTime.String()}).Infof("Successfully backup pvc/%s", pvc)
-	return time.Now().Sub(beginTime), nil
+	return nil
 }
