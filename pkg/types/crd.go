f81d51cdf32437f0b73aacaa763164061fed7e8e
hybfkuf <fredjoseph863@gmail.com>
2022-09-27 10:12:32 +0800

    Add variables about group version

---
+++ pkg/types/crd.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	networkingv1alpha1 "github.com/forbearing/horus-operator/apis/networking/v1alpha1"
+	storagev1alpha1 "github.com/forbearing/horus-operator/apis/storage/v1alpha1"
+)
+
+var (
+	KindBackup    = "Backup"
+	KindRestore   = "Restore"
+	KindClone     = "Clone"
+	KindMigration = "Migration"
+	KindTraffic   = "Traffic"
+
+	ResourceBackup    = "backups"
+	ResourceRestore   = "restores"
+	ResourceClone     = "clones"
+	ResourceMigration = "migrations"
+	ResourceTraffic   = "traffics"
+
+	GroupStorage    = storagev1alpha1.GroupVersion.Group
+	GroupNetworking = networkingv1alpha1.GroupVersion.Group
+
+	GroupVersionStorage = storagev1alpha1.GroupVersion
+	GroupVersionNetworking = networkingv1alpha1.GroupVersion
+)
