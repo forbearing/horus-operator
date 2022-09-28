c152d621f521f587dc6061d3110ccaf8faac7a41
hybfkuf <fredjoseph863@gmail.com>
2022-09-28 16:43:40 +0800

    Add cronjob template

---
+++ pkg/template/cronjob_backup.go
@@ -0,0 +1,39 @@
+package template
+
+var (
+	HorusctlForBackup = `
+apiVersion: batch/v1
+kind: CronJob
+metadata:
+  name: backup-{{.ObjectMeta.Name}}
+  namespace: default
+spec:
+  schedule: '*/1 * * * *'
+  successfulJobsHistoryLimit: 1
+  failedJobsHistoryLimit: 1
+  concurrencyPolicy: Forbid
+  suspend: false
+  jobTemplate:
+    spec:
+      template:
+        spec:
+          containers:
+          - command:
+            - horusctl
+            args:
+            - --log-level={{.Spec.LogLevel}}
+            - --log-format={{.Spec.LogFormat}}
+            - backup
+            - --namespace={{.ObjectMeta.Namespace}}
+            - {{.Spec.BackupFrom.Name}}
+            env:
+            - name: TZ
+              value: {{.Spec.TimeZone}}
+            image: hybfkuf/horusctl:latest
+            imagePullPolicy: Always
+            name: horusctl
+          restartPolicy: Never
+          serviceAccount: horusctl
+          serviceAccountName: horusctl
+`
+)
